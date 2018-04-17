49e0a527196d2718e06d5b644282823710406072
robin torn <robin.torn.98@gmail.com>
2018-04-17 15:50:20 +0200

    affix navbar

--- app/js/custom/index.js
+++ app/js/custom/index.js
@@ -1,25 +1,35 @@
-
-$('.awesome-tooltip').tooltip({
-    placement: 'bottom'
-});  
-/* CSS Tricks smooth scrolling : https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {                   
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 500);
-            return false;
+$(document).ready(function() {
+    
+      var toggleAffix = function(affixElement, scrollElement, wrapper) {
+      
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+        
+        if (scrollElement.scrollTop() >= top){
+            wrapper.height(height);
+            affixElement.addClass("affix");
         }
-    }
-});    
-
-
-/* affix the navbar after scroll below header */
-$('#mainnav').affix({
-offset: {
-top: $('#home').offset().top
-}
-});
+        else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+        }
+          
+      };
+      
+    
+      $('[data-toggle="affix"]').each(function() {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+        
+        ele.before(wrapper);
+        $(window).on('scroll resize', function() {
+            toggleAffix(ele, $(this), wrapper);
+            let nav = $('#first');
+            nav.append(wrapper);
+        });
+        
+        // init
+        toggleAffix(ele, $(window), wrapper);
+      });
+      
+    });
